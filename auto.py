5c1f01410ac5a635a63607a8202e57da4a0f480b
tygr <mauro.mo.tsai@pwc.com>
2021-03-19 14:14:49 +0800

    add make report.

--- auto.py
+++ auto.py
@@ -16,6 +16,32 @@ def load_rule() -> pd.DataFrame:
     windows_rule_filename = IniHelper.get_instance().get_value(E_INI_Session.RULE,E_INI_KEY.WINDOWS_FILE_NAME)
     csv = pd.read_csv(windows_rule_filename)
     return csv
+
+def get_computer_setting(df,computer_name):
+    return df.loc[df['ComputerName'] == computer_name]
+
+def current_computer_setting_at_current_rule_tag(current_rule_tag,computer_settings):
+    computer_settings_size = min(computer_settings.count())
+    for row in computer_settings.iterrows():
+        policy = row[1]['Policy']
+        setting = row[1]['Setting']
+        compared = row[1]['Compared Result']
+        # print (policy,setting,compared,current_rule_tag)
+        for tag in current_rule_tag:
+            if policy == tag.replace('\n',''):
+                computer_settings = setting
+                compared_results = compared
+                return (computer_settings,compared_results)
+    return (None, None)
+
+def get_policy_wording(operations,words):
+    if len(words) != len(operations):
+        raise ArgumentError("input size is not match.")
+    results = []
+    for idx,operation in enumerate(operations):
+        results.append(("%s %s"%(operation,words[idx])))
+    return results
+
 def step1_extract_security_setting_from_htmls(output_filename='raw_data.csv'):
     print ('Running Step 1 ....')
     path = IniHelper.get_instance().get_value(
@@ -90,9 +116,49 @@ def step2_check_all_setting_is_ok_or_not(src_filename,output_filename):
 
     # TODO : use to_csv compared result to save as csv file.
     pass
+
 def step3_make_report(src_filename,output_filename):
     # TODO : call my make report python script (may don't have time to refactoring).
     print ('Running Step 3 ....')
+    rule_config_path = IniHelper.get_instance().get_value(E_INI_Session.RULE,E_INI_KEY.WINDOWS_FILE_NAME)
+
+    df_rule = pd.read_csv(rule_config_path)
+    df_rawdata_with_compared_result = pd.read_csv(src_filename)
+
+    computer_names = computers = list(set(df_rawdata_with_compared_result['ComputerName']))
+    datatable = []
+    
+    for idx_rule in range(len(df_rule['Name'])):
+        
+        row_data = []
+        row_compare_result = []
+        for computer_name in computer_names:
+
+            # TODO : get current computer setting at current rule tag
+            (computer_settings,compared_results) = current_computer_setting_at_current_rule_tag(
+                current_rule_tag = [df_rule['ch'][idx_rule],df_rule['en'][idx_rule]],
+                computer_settings= get_computer_setting(df_rawdata_with_compared_result,computer_name))
+
+            if computer_settings == None:
+                row_data.append("NAN")
+                row_compare_result.append('NAN')
+            else:
+                row_data.append(computer_settings)
+                row_compare_result.append(compared_results)
+        
+        datatable.append(row_data)
+
+    pd_dt = pd.DataFrame(datatable,columns=computer_names)
+    rule_names = list(df_rule['Name'])
+    pd_dt.insert(0,'Setting Name',list(df_rule['Name']))
+    pd_dt.insert(1,'Rule',get_policy_wording(operations=df_rule['operations'],words=df_rule['rule_sec']))
+    # print (pd_dt)
+
+    pd_dt_translation = pd_dt.T
+    # pd_dt_translation.to_csv(output_filename,header=False)
+    pd_dt_translation.to_csv('roadmap_v2.csv',header=False)
+    # print (pd_dt_translation)
+
     pass
 
 def main():
